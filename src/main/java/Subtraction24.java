50c6af1f8457135d42b3c5dcc29701d1e6d0ac4b
Ravula Manasa <manasareddyravula13@gmail.com>
2024-03-20 15:23:34 +0530

    Submittig the code'

---
+++ src/main/java/Subtraction24.java
@@ -0,0 +1,27 @@
+package com.qualizeal.javaapp.mathematics;
+
+public class Subtraction24 {
+	public int sub1() {
+		int num1 = 10;
+		int num2 = 15;
+
+		int result = num1 - num2;
+
+		return result;
+}
+	public void sub2() {
+		int num3 = 5;
+		int num4 = 15;
+
+		int result = num3 - num4;
+
+		System.out.println(result);
+}
+	public int sub3(int num3,int num4) {
+
+		int result = num3 - num4;
+
+		return result;
+}
+
+}
---
+++ src/main/java/com/qualizeal/javaapp/App.java
@@ -0,0 +1,18 @@
+package com.qualizeal.javaapp;
+
+import com.qualizeal.javaapp.mathematics.Subtraction24;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Subtraction24 subtraction24  = new Subtraction24();
+	System.out.println(subtraction24.sub1());
+	
+	subtraction24.sub2();
+	
+	System.out.println(subtraction24.sub3(20,15));
+		
+
+    }
+}
